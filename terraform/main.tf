57305e92b7faef7220fddcc5e2a5b6c2b6f646fd
Jared Ready <jared@jaredready.com>
2022-01-09 21:28:02 -0600

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "aws.sandbox-aft@jaredready.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    SSOUserEmail              = "jared@jaredready.com"
+    SSOUserFirstName          = "Jared"
+    SSOUserLastName           = "Ready"
   }
 
   account_tags = {
